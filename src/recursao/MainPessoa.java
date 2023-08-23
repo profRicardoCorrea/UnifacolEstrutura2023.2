4c812793384a510bb245557ee4ca8d6ded6e02db
Ricardo Corres <111370109+profRicardoCorrea@users.noreply.github.com>
2023-08-22 21:11:13 -0300

    aula classe pessoa

--- src/recursao/MainPessoa.java
+++ src/recursao/MainPessoa.java
@@ -1,5 +1,21 @@
 package recursao;
 
 public class MainPessoa {
-
+	public static void main(String[] args) {
+		Pessoa p1=new Pessoa();
+		p1.nome="Falono";
+		
+		Pessoa p2=new Pessoa();
+		p2.nome="Ciclano";
+		p1.filho=p2;
+		Pessoa p3=new Pessoa();
+		p1.filho.filho=p3;
+		p2.filho=p3;
+		Pessoa p4= new Pessoa();
+		p4.nome="Beltrano";
+		p3.filho=p4;
+		Pessoa p5=new Pessoa();
+		p5.nome="Fulano Filho";
+		p4.filho=p5;
+	}
 }
