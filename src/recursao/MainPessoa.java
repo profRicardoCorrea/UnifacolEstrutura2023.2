8866a798d6ba4d9a12c5ad06fb290c6b4f273569
Ricardo Corres <111370109+profRicardoCorrea@users.noreply.github.com>
2023-09-05 19:33:36 -0300

    metodo

--- src/recursao/MainPessoa.java
+++ src/recursao/MainPessoa.java
@@ -1,7 +1,13 @@
 package recursao;
 
 public class MainPessoa {
-	
+	public static Pessoa add_filho(Pessoa matriarca, Pessoa p) {
+		if(matriarca==null)
+			matriarca=p;
+		else
+			matriarca.filho=add_filho(matriarca.filho, p);
+		return matriarca;
+	}
 	public static void imprimir_pai_filho(Pessoa p) {
 		if(p!=null)
 			System.out.print(">> Pai:"+p.nome );
@@ -15,25 +21,25 @@ public class MainPessoa {
 	
 	
 	public static void main(String[] args) {
-		Pessoa p1=new Pessoa();
+		/*Pessoa p1=new Pessoa();
 		p1.nome="Falono";		
 		Pessoa p2=new Pessoa();
 		p2.nome="Ciclano";
-		p1.filho=p2;
+		//p1.filho=p2;
 		Pessoa p3=new Pessoa();
-		p1.filho.filho=p3;
-		p2.filho=p3;
-		p1.filho.filho=p3;
+		//p1.filho.filho=p3;
+		//p2.filho=p3;
+		//p1.filho.filho=p3;
 		Pessoa p4= new Pessoa();
 		p4.nome="Beltrano";
-		p3.filho=p4;
-		p1.filho.filho.filho=p4;
+		//p3.filho=p4;
+		//p1.filho.filho.filho=p4;
 		Pessoa p5=new Pessoa();
 		p5.nome="Fulano Filho";
-		p4.filho=p5;
-		p1.filho.filho.filho.filho=p5;
-		imprimir_pai_filho(p1);
-		
+		//p4.filho=p5;
+		//p1.filho.filho.filho.filho=p5;
+		//imprimir_pai_filho(p1);
+		*/
 		/*System.out.println(p1.nome);
 		System.out.println(p1.filho.nome);
 		System.out.println(p1.filho.filho.nome);
@@ -41,7 +47,7 @@ public class MainPessoa {
 		System.out.println(p1.filho.filho.filho.filho.nome);
 		*/
 		//imprimir_pai_filho(p1);
-		/* familia=null;
+		Pessoa familia=null;
 		Pessoa p1=new Pessoa();
 		p1.nome="Falono";
 		familia=add_filho(familia, p1);
@@ -61,6 +67,6 @@ public class MainPessoa {
 		Pessoa p5=new Pessoa();
 		p5.nome="Fulano Filho";
 		familia=add_filho(familia, p5);
-		imprimir_pai_filho(familia);*/
+		imprimir_pai_filho(familia); 
 	}
 }
